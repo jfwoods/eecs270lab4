0b78f8da9d07f6a372f9d5274fb19f7738412bba
John Woods <jfwoods@umich.edu>
2024-10-10 18:55:11 -0400

    stopped being silly

--- ADDER.v
+++ ADDER.v
@@ -3,7 +3,7 @@ input a, b, inC;
 output ouC, s;
 
 assign s = a ^ b ^ inC;
-assign ouC = (a&b) + (a ^ b)&inC;
+assign ouC = (a&b) | (a ^ b)&inC;
 endmodule
 
 
@@ -23,26 +23,20 @@ assign absA = OP[2] & OP[1];
 assign absB = ~OP[2] & OP[1];
 
 
-wire ctrl; //1 = subtract, 0 = add
 wire c01, c12, c23; //first num is CIN, 2nd is COUT
-assign ctrl = ~(add | absA | absB);
 
 //Putting vals into registers
 assign A = ~bsuba | add ? V1 : V2;
 assign B = ~bsuba | add ? V2 : V1;
 
-reg [3:0] C;
-assign C = B ^ ctrl;
-
-fullAdder add0(A[0], C[0], ctrl, c01, R[0]);
-fullAdder add1(A[1], C[1], c01, c12, R[1]);
-fullAdder add2(A[2], C[2], c12, c23, R[2]);
-fullAdder add3(A[3], C[3], c23, ovf, R[3]);
+fullAdder add0(A[0], B[0], ctrl, c01, R[0]);
+fullAdder add1(A[1], B[1], c01, c12, R[1]);
+fullAdder add2(A[2], B[2], c12, c23, R[2]);
+fullAdder add3(A[3], B[3], c23, ovf, R[3]);
 
 initial begin
     A = 4b'0000;
     B = 4b'0000;
-    C = 4b'0000;
 end
 endmodule
 
