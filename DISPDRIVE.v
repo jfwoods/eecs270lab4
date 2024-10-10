a495471ad9ed7f248a33eb98524ed6fa46300952
John Woods <jfwoods@umich.edu>
2024-10-10 17:17:54 -0400

    numDisp implementation

--- DISPDRIVE.v
+++ DISPDRIVE.v
@@ -1,5 +1,34 @@
 module numDisp(A, H1, H0); //display 1 4-bit 2's c num
+input [3:0] A;
+output [6:0] H0;
+output [6:0] H1;
 
+reg [6:0] NEG; // "-" character
+reg [6:0] CHARS[0:15];
+
+initial
+begin
+    CHARS[0] = 7'b1000000;
+    CHARS[1] = 7'b1111001;
+    CHARS[2] = 7'b0100100;
+    CHARS[3] = 7'b0110000;
+    CHARS[4] = 7'b0011001;
+    CHARS[5] = 7'b0010010;
+    CHARS[6] = 7'b0000010;
+    CHARS[7] = 7'b1111000;
+    CHARS[8] = 7'b0000000;
+    CHARS[9] = 7'b1111000; //7
+    CHARS[10] = 7'b0000010;
+    CHARS[11] = 7'b0010010;
+    CHARS[12] = 7'b0011001; //4
+    CHARS[13] = 7'b0110000;
+    CHARS[14] = 7'b0100100;
+    CHARS[15] = 7'b1111001; //1
+    NEG = 7'b0111111;
+end
+
+assign H0 = A[3] ? NEG : 7'b1111111;
+assign H1 = CHARS[A];
 endmodule
 
 module ovfDisp(ovf, H0);
