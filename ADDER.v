149cf94880d7aa544bcf81829952ceddfc2d4c85
John Woods <jfwoods@umich.edu>
2024-10-11 02:56:09 -0400

    ADDER and absVal

--- ADDER.v
+++ ADDER.v
@@ -17,24 +17,28 @@ endmodule
 
 
 module ADDER(X, Y, R, ovf);
-input [3:0] V1, V2;
+input [3:0] X;
+input [3:0] Y;
 output [3:0] R;
 output ovf;
 
 wire c01, c12, c23; //first num is CIN, 2nd is COUT
+wire cin;
 
-fullAdder add0(X[0], Y[0], ctrl, c01, R[0]);
+fullAdder add0(X[0], Y[0], cin, c01, R[0]);
 fullAdder add1(X[1], Y[1], c01, c12, R[1]);
 fullAdder add2(X[2], Y[2], c12, c23, R[2]);
 fullAdder add3(X[3], Y[3], c23, ovf, R[3]);
 
 endmodule
 
-module absVal(A, R, error);
+module absVal(A, R);
 input [3:0] A;
 output [3:0] R;
-output error;
 
-wire sign; assign sign = X[3]; //if 0, R = A. if 1, invert A and add 4b'0001 
+wire sign; assign sign = A[3]; //if 0, R = A. if 1, invert A and add 4b'0001 
+wire [3:0] twoComp;
+twosComp twoC(A, twoComp);
 
+assign R = sign ? twoComp : A;
 endmodule
