7ab94d2fae1d711045d6ee6ee595b91240840e69
John Woods <jfwoods@umich.edu>
2024-10-11 02:36:05 -0400

    add twos compliment module, make prefix module for opcode

--- ADDER.v
+++ ADDER.v
@@ -6,24 +6,27 @@ assign s = a ^ b ^ inC;
 assign ouC = (a&b) | (a ^ b)&inC;
 endmodule
 
+module twosComp(X, R);
+    input [3:0] X;
+    output [3:0] R;
+    wire [3:0] xInv;
+    assign xInv[3:0] = ~X[3:0];
+    wire ovf;
+    ADDER add1(X, 4b'0001, R, ovf);
+endmodule
 
-module ADDER(V1, V2, R, ovf);
+
+module ADDER(X, Y, R, ovf);
 input [3:0] V1, V2;
 output [3:0] R;
 output ovf;
-wire [3:0] A;
-wire [3:0] B;
 
 wire c01, c12, c23; //first num is CIN, 2nd is COUT
 
-//Putting vals into buses
-assign A = ~bsuba | add ? V1 : V2;
-assign B = ~bsuba | add ? V2 : V1;
-
-fullAdder add0(A[0], B[0], ctrl, c01, R[0]);
-fullAdder add1(A[1], B[1], c01, c12, R[1]);
-fullAdder add2(A[2], B[2], c12, c23, R[2]);
-fullAdder add3(A[3], B[3], c23, ovf, R[3]);
+fullAdder add0(X[0], Y[0], ctrl, c01, R[0]);
+fullAdder add1(X[1], Y[1], c01, c12, R[1]);
+fullAdder add2(X[2], Y[2], c12, c23, R[2]);
+fullAdder add3(X[3], Y[3], c23, ovf, R[3]);
 
 endmodule
 
@@ -32,6 +35,6 @@ input [3:0] A;
 output [3:0] R;
 output error;
 
-wire sign; assign sign = A[3]; //if 0, R = A. if 1, invert A and add 4b'0001 
+wire sign; assign sign = X[3]; //if 0, R = A. if 1, invert A and add 4b'0001 
 
 endmodule
--- Calculator.v
+++ Calculator.v
@@ -6,13 +6,33 @@ module Calculator(KEY, SW, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX0);
    output [6:0] HEX3, HEX2;
    output [6:0] HEX0; 
 
-//opcode decoding
-   wire add, asubb, bsuba, absA, absB;
+   wire [3:0] A, B;
+   assign A[3:0] = SW[7:4];
+   assign B[3:0] = SW[3:0];
+
+
+
+
+
+   
+
+endmodule
+
+module Prefix(KEY, A, B, X, Y, cIN);
+   input [2:0] KEY;
+   input [3:0] A, B;
+   output [3:0] X, Y;
+   output cIN;
+
+   //opcode decoding
+   wire add, asubb, bsuba, absA, absB, sub, abs;
    assign add = (~KEY[0] & ~KEY[1] & ~KEY[2]) | (~KEY[0] & ~KEY[1] & KEY[2]);
    assign asubb = KEY[0] & ~KEY[1] & ~KEY[2];
    assign bsuba = KEY[0] & ~KEY[1] & KEY[2];
    assign absA = KEY[2] & KEY[1];
    assign absB = ~KEY[2] & KEY[1];
+   assign sub = asubb | bsuba;
+   assign abs = absA | absB;
 
    
 
